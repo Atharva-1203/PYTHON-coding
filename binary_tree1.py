f9e500fb9bfe0b9d05a3fe461c44581bafc2f5c3
Atharva-1203 <atharvaadsul0608@gmail.com>
2026-08-21 11:21:59 +0530

    Solved L144

---
+++ binary_tree1.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+root=Node(5)
+root.left=Node(3)
---
+++ l144.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def __init__(self):
+        self.ans=[]
+    def preorder(self,root):
+        #Base case
+        if root is None:
+            return 
+        #Recusrion called for preorder traversal
+        self.ans.append(root.val)
+        self.preorder(root.left)
+        self.preorder(root.right)
+    def preorderTraversal(self, root):
+        self.ans=[]
+        self.preorder(root)
+        return self.ans
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        
