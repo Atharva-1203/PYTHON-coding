bb8220c7424a2d4d7e974a8c74595ae54e703880
Atharva-1203 <atharvaadsul0608@gmail.com>
2026-08-26 18:12:29 +0530

    Solved few leetcode problems

---
+++ DSA/stack1.py
@@ -0,0 +1,1 @@
+#Basic operations of stack: push, pop, top, isEmpty, isFull
---
+++ Leetcode/L-509_DP.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def rec(self,n,dp):
+        if n==0 or n==1:
+            return n
+        if dp[n]!=-1:
+            return dp[n]
+        dp[n] = self.rec(n - 1, dp) + self.rec(n - 2, dp)
+        return dp[n]
+
+    def fib(self, n):
+        dp=[-1]*(n+1)
+
+        return self.rec(n,dp)
+        """
+        :type n: int
+        :rtype: int
+        """
+        
